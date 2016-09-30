0f2a46ecf4ae0423cefba06bda38606c12d8aa3f
Martín Delafuente <Martn Delafuente@HPENVYI7WIN10>
2016-09-30 00:10:29 -0300

    Consumo de servicio de la intendencia

--- AgenciaServidorCentral/src/comunicacion/socketServer.java
+++ AgenciaServidorCentral/src/comunicacion/socketServer.java
@@ -1,13 +1,60 @@
 package comunicacion;
-import java.net.*;
-//importar la libreria java.net
-
-import java.io.*;
 //importar la libreria java.io
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
+import Controladores.ControladorTicketProxy;
+import Controladores.TicketVentaEntrada;
+import Controladores.TicketVentaSalida;
 
 // declaramos la clase servidortcp
 
 public class socketServer {
+	
+	public TicketVentaEntrada crearMensajeCompraTicket(String msg){
+		TicketVentaEntrada tve = new TicketVentaEntrada();
+		
+		
+		
+		return tve;
+	}
+	
+	
+	public String getConfigPropertyValue(String propStr) throws IOException{
+		InputStream inputStream = null;
+		String result = null;
+		try {
+			Properties prop = new Properties();
+			String propFileName = "config.properties";
+
+			//inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+			inputStream = classloader.getResourceAsStream(propFileName);
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+			
+			result = prop.getProperty(propStr);
+		}
+		catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			if(inputStream != null)
+			inputStream.close();
+		}
+		
+		return result;
+	}
+	
 
 	// método principal main de la clase
 	public static void main(String argv[]) {
@@ -20,6 +67,9 @@ public class socketServer {
 		// Declaramos un bloque try y catch para controlar la ejecución del subprograma
 		try {
 
+			socketServer ss = new socketServer();
+			String s = ss.getConfigPropertyValue("idAgencia");
+			
 			// Instanciamos un ServerSocket con la dirección del destino y el
 			// puerto que vamos a utilizar para la comunicación
 
@@ -39,13 +89,49 @@ public class socketServer {
 				DataOutputStream out =
     					new DataOutputStream(socket_cli.getOutputStream());
 
-				// Creamos un bucle do while en el que recogemos el mensaje
-				// que nos ha enviado el cliente y después lo mostramos
-				// por consola
-
+				
 				do {
 					String mensaje ="";
 					mensaje = in.readUTF();
+					if (mensaje!= null){
+						StringTokenizer st = new StringTokenizer(mensaje,"|");
+						String comando =  st.nextToken();
+						if (comando.equals("Login")){
+							//Crea mensaje de login
+							System.out.println("Comienza con Login");
+							
+						}
+						else {
+	
+							if (comando.equals("Compra")){
+								TicketVentaEntrada tve = new TicketVentaEntrada(); 
+								tve.setAgenciaNro(1);
+								comando =  st.nextToken();
+								tve.setMatricula(comando);
+								comando =  st.nextToken();
+								tve.setCantMin(Integer.parseInt(comando));
+								//SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+								//tve.setFchHraEst(df.parse(msg[3]));									
+								//tve.setFchHraVenta(new Date());							
+								ControladorTicketProxy ctp = new ControladorTicketProxy();
+								TicketVentaSalida tvs = new TicketVentaSalida();
+								tvs = ctp.venderTicket(tve);
+								
+								System.out.println("Info del ticket");
+								System.out.println(tvs.getImpTotal());
+								System.out.println(tvs.getTicketNro());
+								System.out.println(tvs.getMensaje());
+							}
+							else 
+							{
+								if (comando.equals("Anula")){
+									//Crea mensaje anula ticket
+									System.out.println("Comienza con Anula");
+								}
+							}
+						}
+					}
+
 					out.writeUTF(mensaje);
 					System.out.println(mensaje);
 				} while (1>0);
